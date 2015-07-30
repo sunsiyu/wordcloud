991c127cb2c95d21c9edf4cf2a69b0ceb97bc991
Siyu Sun <sunsiyu.tud@gmail.com>
2015-07-30 23:32:05 +0200

    first min. prototype.

--- server.R
+++ server.R
@@ -2,7 +2,30 @@
 
 library(shiny)
 
-shinyServer(function(input, output) {
-   
+function(input, output, session){
   
-})
+  # Define a reactive expression for the document term matrix
+  terms <- reactive({
+    
+    # Change when the "update" button is pressed...
+    input$update
+    # ... but not for anything else
+    isolate({
+      withProgress({
+        setProgress(message = "Processing corpus...")
+        getTermMatrix(input$selection)
+      })
+    })
+  })
+  
+  wordcloud_rep <- repeatable(wordcloud)
+  
+  output$plot <- renderPlot({
+    
+    v <- terms()
+    wordcloud_rep(names(v), v, scale = c(4, 0.5),
+                  min.freq = input$freq, max.words = input$max, 
+                  colors = brewer.pal(8, "Dark2"))
+  })
+  
+}
--- ui.R
+++ ui.R
@@ -1,16 +1,24 @@
 ## ui.R ##
-
+library(shiny)
 fluidPage(
   
   titlePanel("My Word Cloud"),
   
   sidebarLayout(
     
-    # Add items
-    
-    actionButton("update", "Change")
-    
-  ),
+    sidebarPanel(
+      selectInput("selection", "Choose a txt: ",
+                  choices = books),
+      actionButton("update", "Change"),
+      hr(),
+      sliderInput("freq", "Minimum Frequency: ", 1, 50, 15),
+      sliderInput("max", "Maximum No. of words: ", 1, 300, 100)
+      
+    ),
   
-  mainPanel("hello",width=6)
+    # Show Word Cloud
+    mainPanel(
+      plotOutput("plot")
+    )
+  )
 )
